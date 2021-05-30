1d4284d23b5208dec55ba0498325c0f54c415e62
Grizz03 <steve.grizz03@gmail.com>
2021-05-29 21:38:15 -0700

    char and boolean notes

---
+++ src/com/company/CharBoolean.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CharBoolean {
+    public static void main(String[] args) {
+        // Char can only hold one character or unicode
+        char myChar = 'S';
+        char myUnicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+
+        // Boolean
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+        boolean isCustomerOver18 = true;
+    }
+}
