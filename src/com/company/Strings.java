2fed095ab9d9885fb28c691d065b92423eeac50d
Grizz03 <steve.grizz03@gmail.com>
2021-05-29 21:53:55 -0700

    string notes

---
+++ src/com/company/Strings.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Strings {
+    public static void main(String[] args){
+        // Strings || \u0000 for unicode || immutable once created
+        String myString = "This is a string";
+        System.out.format("my string is is equal to %s", myString);
+    }
+}
