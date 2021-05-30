cad1850e378d7da6a2c987eee43f4778f95b17a9
Grizz03 <steve.grizz03@gmail.com>
2021-05-30 12:43:15 -0700

    simple if/else statement in java

---
+++ src/com/company/IfThenStatements.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class IfThenStatements {
+
+    public static void main(String[] args) {
+        // simple example of if/else statement
+        boolean isAlien = true;
+        if (!isAlien)
+            System.out.println("It is not an alien");
+        else {
+            System.out.println("It is an alien!");
+        }
+    }
+}
