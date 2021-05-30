2ea9e630ba18f7e371ba6805766e6a214915b558
Grizz03 <steve.grizz03@gmail.com>
2021-05-30 12:25:35 -0700

    notes on expressions(same as other languages)

---
+++ src/com/company/Expressions.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Expressions {
+    public static void main(String[] args){
+        // Expressions, Operators and Operands ->
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("one plus two = " + result);
+
+        result = result - 1;
+        System.out.println(result);
+
+        result = result * 10;
+        System.out.println(result);
+
+        result = result / 5; // 20 / 5 = 4
+        System.out.println(result);
+
+        result = result % 3; // remainder of (4 / 3) = 1
+        System.out.println(result);
+    }
+}
