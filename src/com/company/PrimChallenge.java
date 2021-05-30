505e4de73415119ec6d020ec14d1444da2086023
Grizz03 <steve.grizz03@gmail.com>
2021-05-29 21:11:49 -0700

    small challenge with equation

---
+++ src/com/company/PrimChallenge.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class PrimChallenge {
+    public static void main(String[] args) {
+        // Convert pounds to kilograms
+        float numPounds = 180f;
+        float convertToKg = numPounds * 0.45359237f;
+        System.out.format("%.1f pounds equal %.1f kilograms", numPounds, convertToKg);
+    }
+}
