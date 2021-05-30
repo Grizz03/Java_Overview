6c648d12fa38e158e5c5abdc50cf20699ba06ffa
Grizz03 <steve.grizz03@gmail.com>
2021-05-30 12:57:53 -0700

    notes on logical OR operator

--- src/com/company/IfThenStatements.java
+++ src/com/company/IfThenStatements.java
@@ -11,14 +11,17 @@ public class IfThenStatements {
             System.out.println("It is an alien!");
         }
 
-        int topScore = 81;
+        int topScore = 80;
         if (topScore >= 100) {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 80;
+        int secondTopScore = 81;
+        // can do same thing as &&(and operator) with or(||) and it will run because topScore is less than 100, only one condition has to be true.
         if (topScore > secondTopScore && topScore < 100) {
             System.out.println("Greater than second top score and less than 100");
         }
+
+
     }
 }
